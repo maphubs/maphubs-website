74589dac366b7da2a51d2d5f98501db2418495c0
Kristofor Carle <kris@maphubs.com>
2016-10-26 01:45:20 -0400

    fix google analytics

--- services/express-react-views.js
+++ services/express-react-views.js
@@ -243,7 +243,7 @@ function createEngine(engineOptions) {
             `;
 
         }
-      if(process.env.NODE_ENV == 'production' && !local.disableTracking){
+      if(process.env.NODE_ENV == 'production'){
         markup += `
         <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
