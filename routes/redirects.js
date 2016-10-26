3fabdf688eaa411e19f7419c96589dc654448abb
Kristofor Carle <kris@maphubs.com>
2016-10-26 00:25:50 -0400

    fix redirect urls

--- routes/redirects.js
+++ routes/redirects.js
@@ -4,31 +4,31 @@ module.exports = function(app) {
 
 //Subpages
 app.get('/layers', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/maps', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/groups', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/hubs', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/stories', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/explore', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/search', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 
@@ -36,47 +36,47 @@ app.get('/search', function(req, res) {
 //API and 3rd+ level pages
 
 app.get('/api/*', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/tiles/*', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/layer/*', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/feature/*', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/map/*', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/user/*', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/hub/*', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/story/*', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/group/*', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/image/*', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 app.get('/raster/*', function(req, res) {
-  return res.redirect(301, config.redirectDomain + '/' + req.originalUrl);
+  return res.redirect(301, config.redirectDomain + req.originalUrl);
 });
 
 
